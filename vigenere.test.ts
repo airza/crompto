4dafb066e93d7f81fb70225282453c38bfe0ef20
Nemithrag <aj@genji.home>
2023-11-03 16:43:08 +0100

    starting vignere code + test

--- vigenere.test.ts
+++ vigenere.test.ts
@@ -1,4 +1,17 @@
-import {test} from "bun:test";
+import { expect, test } from "bun:test";
+import cipherText from "./ciphertexts/cipher.txt";
+import cipherText2 from "./ciphertexts/cipher2.txt";
+import english from "./references/english.txt";
+import dutch from "./references/dutch.txt";
+import {indexCodeword} from "./vigenere.ts"
+
+console.log("----------------------------------------------------")
+
+test("Indexing codeword works", ()=>{
+    expect(indexCodeword(parameter1, parameter2)).toEqual(outcome)
+})
+
+//expect(isDutchOrEnglish(dutch, false)).toEqual("dutch");
 
 test("Encrypting works",()=> {
 
