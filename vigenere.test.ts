6e8a915000e616d55c1d5bb0c89c93f4fb165c6f
Nemithrag <aj@genji.home>
2023-11-15 16:39:01 +0100

    aaaaaaaaaa

--- vigenere.test.ts
+++ vigenere.test.ts
@@ -1,12 +1,18 @@
 import { expect, test } from "bun:test";
-import {decryptVigenere, encryptVigenere, getEncryptRotation, rotatingLetters} from "./vigenere.ts"
+import {
+    decryptVigenere,
+    encryptVigenere,
+    getEncryptRotation, getTextSubset, hasEvilCharacters,
+    rotatingLetters,
+    splitTextCodewordwise
+} from "./vigenere.ts"
 
 console.log("----------------------------------------------------")
 
 test("Getting encryption rotation works",()=>{
     expect(getEncryptRotation("ABBA")).toEqual([0, 1, 1, 0])
     expect(getEncryptRotation("nop")).toEqual([13, 14, 15])
-    expect(getEncryptRotation("DON'T")).toEqual([3, 14, 13, 19])
+    expect(getEncryptRotation("DON'T")).toThrow('evil')
 })
 
 test("Rotating letters works",()=>{
@@ -18,10 +24,29 @@ test("Encrypting works",()=> {
     expect(encryptVigenere("hello", "nop")).toEqual("USAYC")
     expect(encryptVigenere("HELLO", "CODEWORD")).toEqual("JSOPK")
     expect(encryptVigenere("HELLO BITCH", "NOP")).toEqual("USAYCQVHRU")
-    expect(encryptVigenere("HELLO", "KILL ME")).toEqual("RMWWA")
-    expect(encryptVigenere("H???", "DAY!")).toEqual("K")
+    expect(encryptVigenere("HELLO", "KILL ME")).toThrow('evil')
+    expect(encryptVigenere("H???", "DAY!")).toThrow('evil')
 })
 
-test("Decrypting works", ()=>{
+test("Decrypting with known codeword works", ()=>{
     expect(decryptVigenere("USAYC QVHRU", "NOP")).toEqual("HELLOBITCH")
+})
+
+test("Splitting text into subset based on a single codeword character works", ()=>{
+    expect(getTextSubset("HELPO", 4, 0)).toEqual("HO")
+    expect(getTextSubset("HELPO", 4, 2)).toEqual("L")
+    expect(getTextSubset("HELPO", 3, 1)).toEqual("EO")
+})
+
+test("Splitting the text into all subsets based on full codeword works", ()=>{
+    expect(splitTextCodewordwise("VIGNEREST", "HELP")).toEqual(["VET", "IR", "GE", "NS"])
+    expect(splitTextCodewordwise("VIGNEREST", "KILLME")).toEqual(["VE", "IS", "GT", "N", "E", "R"])
+    expect(splitTextCodewordwise("TSUKIMI", "KILLME")).toEqual(["TI", "S", "U", "K", "I", "M"])
+    expect(splitTextCodewordwise("VIGNEREST", "X")).toThrow('ceasar')
+})
+
+test("Testing codeword for evil characters works?", ()=>{
+    expect(hasEvilCharacters("KILLME")).toEqual(false)
+    expect(hasEvilCharacters("KILLME?")).toEqual(true)
+
 })
--- vigenere.ts
+++ vigenere.ts
@@ -1,12 +1,21 @@
 import {encryptCaesar} from "./caesar";
+import _ from 'lodash';
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+// todo fix capital letters in splitCodewordWise and getTextSubset
+// todo throw an error when there's a number where there shOULDNT BE
+// todo allow spaces in code word
 
 export function encryptVigenere(text:string, codeword:string): string{
     return rotatingLetters(text, getEncryptRotation(codeword))
 }
 
 export function decryptVigenere(text:string, codeword:string): string{
+    if (hasEvilCharacters(codeword)) {
+        throw new Error ("Your codeword is evil! >:c");
+    }
     let decryptRotations:number[] = getEncryptRotation(codeword).map((e)=>{
         return e * -1
     })
@@ -14,6 +23,9 @@ export function decryptVigenere(text:string, codeword:string): string{
 }
 
 export function getEncryptRotation(codeword:string): number[]{
+    if (hasEvilCharacters(codeword)) {
+        throw new Error ("Your codeword is evil! >:c");
+    }
     let codewordLettersOnly:string[] = codeword.toUpperCase().split("").filter((e)=>{
         return alphabet.includes(e)
     })
@@ -32,4 +44,32 @@ export function rotatingLetters(text:string, rotations:number[]): string{
         return encryptCaesar(e, currentRotation)
     })
     return outputText.join("")
+}
+
+export function getTextSubset(text:string, codewordLength:number, offset:number):string{
+    let textArray:string[] = text.split("")
+    return (textArray.filter((e, i)=>{
+        return i % codewordLength == offset
+    })
+    .join(""))
+}
+
+export function splitTextCodewordwise(text:string, codeword:string):string[]{
+    if (hasEvilCharacters(codeword)) {
+        throw new Error (`Your codeword ${codeword} is evil! >:c`);
+    }
+    if (codeword.length < 2) {
+        throw new Error (`Codeword ${codeword} makes this a caesar cipher, not a vigenere, you dumb bitch.`);
+    }
+    let codewordArray = codeword.split("")
+    return codewordArray.map((e, i)=>{
+        return getTextSubset(text, codeword.length, i)
+    })
+}
+
+export function hasEvilCharacters(codeword:string):boolean{
+    let codewordArray = codeword.toUpperCase().split("")
+    return (codewordArray.some((e)=>{
+        return !alphabet.includes(e)
+    }))
 }
